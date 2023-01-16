3c1970eee8996088ee131ce13ce8aaaf85787f8f
PiraTechnics <devin1younge@gmail.com>
2023-01-15 18:13:05 -0800

    updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wisdomball
 A small utility to dispense nuggets of wisdom
+
+
+Currently just a CLI utility, run with python - python3 wisdomball.py
+
+Reads off file 'wisdom': 'https://raw.githubusercontent.com/PiraTechnics/wisdomball/main/wisdom'
--- wisdomball.py
+++ wisdomball.py
@@ -4,5 +4,7 @@ import urllib.request
 url = 'https://raw.githubusercontent.com/PiraTechnics/wisdomball/main/wisdom'
 wisdom_list = urllib.request.urlopen(url).read().splitlines()
 
+print("The Ball of Wisdom says:")
+
 random_line = random.randint(0, len(wisdom_list))
 print(wisdom_list[random_line].decode('utf-8'))
