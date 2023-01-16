cf68bb03b5a8295da0085f516b1c81806a76b87d
PiraTechnics <devin1younge@gmail.com>
2023-01-15 18:14:50 -0800

    Update: List Length Index error

--- wisdomball.py
+++ wisdomball.py
@@ -6,5 +6,5 @@ wisdom_list = urllib.request.urlopen(url).read().splitlines()
 
 print("The Ball of Wisdom says:")
 
-random_line = random.randint(0, len(wisdom_list))
+random_line = random.randint(0, len(wisdom_list)-1)
 print(wisdom_list[random_line].decode('utf-8'))
